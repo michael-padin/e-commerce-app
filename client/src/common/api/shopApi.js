0697e0310bbeb8c127c173dc2bd45bec7bb4c659
Michael G. Padin <75446003+moklo0821@users.noreply.github.com>
2022-04-09 13:15:10 +0800

    fix

--- client/src/common/api/shopApi.js
+++ client/src/common/api/shopApi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+require('dotenv').config()  
+
 
 const BASE_URL = "http://localhost:4000/api/"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTZiMmViNzI5NWM0NTY1NzA3NjE2M2YiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MzUyMzc1NTEsImV4cCI6MTYzNTI0MTE1MX0._mZzQQWQqBpQZYm4Y16k_g5bJSaV7OVrS_-raU1fwzw"
+const TOKEN  = process.env.TOKEN;
 export const publicRequest = axios.create({
     baseURL: BASE_URL
 })
