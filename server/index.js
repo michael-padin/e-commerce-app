0b54b03e9e6483b2cae35b6f9a5e4e61865ae397
Michael <padinmichael201@gmail.com>
2023-02-14 13:35:18 +0800

    add http server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import helmet from "helmet"
 dotenv.config();
 import cors from "cors";
 
@@ -9,11 +10,10 @@ import userRoute from "./src/routes/user.js";
 import ProductRoute from "./src/routes/product.js";
 import orderRoute from "./src/routes/order.js";
 import stripeRoute from "./src/routes/stripe.js";
-import * as path from "path";
 
 const app = express();
-const port =  process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
@@ -22,26 +22,8 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-// ------------------ DEPLOYMENT --------------------------------
-
-const __dirname = path.resolve();   
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/', 'build', 'index.html'));
-    })
-} else {
-    app.get("/", (req, res) => {
-        res.send("Api is running");
-    })
-}
-
-// ------------------ DEPLOYMENT --------------------------------
-
 
 // Connect MongoDB at default port 27017.
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB connections successfull")).catch((err) => console.log(err));
 
-app.listen(port, () => console.log(`Server is running at ${port}`));
+export default app
